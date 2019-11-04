8bd206609ac59a55e834617f9c26a98c18f1fefc
Chidi <cjy2k1@yahoo.com>
2019-11-04 17:19:10 +0100

    Updated ProfileActivity

--- app/src/main/java/com/aad/cn47/testaadproject/ProfileActivity.java
+++ app/src/main/java/com/aad/cn47/testaadproject/ProfileActivity.java
@@ -24,8 +24,8 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
     }
 
     @Override
